213090dbcde6a0a6c12ec4301e8c4299d50890bf
Mutiaraannisa <72312213+Mutiaraannisa@users.noreply.github.com>
2020-10-05 23:41:58 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+import "regenerator-runtime";
+import "./styles/style.css";
+import "./script/component/app-bar.js";
+import "./script/component/marque-p.js";
+import "./script/component/flex-container.js";
+import main from "./script/view/main.js";
+import Main from "./script/view/viewIndonesia.js";
+
+document.addEventListener("DOMContentLoaded", main);
+document.addEventListener("DOMContentLoaded", Main);
